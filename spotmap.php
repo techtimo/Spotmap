f9280d80fbe188ca2160f8335ba97fb347898a28
Timo Giese <giese.timo@gmail.com>
2019-06-18 14:32:00 +0200

    Added leaflet as embedded map\n data gets shared via AJAX.

--- spotmap.php
+++ spotmap.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: Spotmap
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Plugin URI: https://github.com/techtimo/spotmap
 Description: A brief description of the Plugin.
 Version: 1.0
 Author: Dell E7240
-Author URI: http://URI_Of_The_Plugin_Author
-License:      GPL2
+Author URI: https://github.com/techtimo
+License: GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 
     Spotmap is free software: you can redistribute it and/or modify
@@ -18,11 +18,12 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Spotmap. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
+//Block direct access
+defined( 'ABSPATH' ) or die();
 
+
+register_activation_hook( __FILE__, 'spotmap_activation' );
 function spotmap_activation(){
     global $wpdb;
     $table_name = $wpdb->prefix."spotmap_points";
@@ -45,45 +46,66 @@ function spotmap_activation(){
 	if ( ! wp_next_scheduled( 'spotmap_cron_hook' ) ) {
 		wp_schedule_event( time(), 'twohalf_min', 'spotmap_cron_hook' );
 	}
-
-
 }
-register_activation_hook( __FILE__, 'spotmap_activation' );
 
 
-function spotmap_deactivation(){
-
-	wp_unschedule_event( time(), 'spotmap_cron_hook' );
-
-}
 register_deactivation_hook( __FILE__, 'spotmap_deactivation' );
+function spotmap_deactivation(){
+    wp_unschedule_event( time(), 'spotmap_cron_hook' );
+}
 
 
+add_filter( 'cron_schedules', 'spotmap_add_cron_interval' );
 function spotmap_add_cron_interval( $schedules ) {
-	$schedules['twohalf_min'] = array(
+    $schedules['twohalf_min'] = array(
 		'interval' => 150,
 		'display'  => esc_html__( 'Every 2.5 Minutes' ),
 	);
-
-	return $schedules;
+    return $schedules;
 }
-add_filter( 'cron_schedules', 'spotmap_add_cron_interval' );
 
 
+add_action('wp_enqueue_scripts', 'spotmap_setup_scripts_and_styles');
+function spotmap_setup_scripts_and_styles() {
+    wp_register_style( 'leafletcss', 'https://unpkg.com/leaflet@1.5.1/dist/leaflet.css' );
+    wp_enqueue_script( 'leafletjs', 'https://unpkg.com/leaflet@1.5.1/dist/leaflet.js');
+	wp_enqueue_script( 'spotmap-handler',  plugins_url( 'public/js/maphandler.js', __FILE__ ),array('jquery'),false,true);
+
+	wp_localize_script( 'spotmap-handler', 'spotmapjsobj', array(
+		'ajax_url' => admin_url( 'admin-ajax.php' )
+	) );
+}
+
+#https://byronyasgur.wordpress.com/2011/06/27/frontend-forward-facing-ajax-in-wordpress/
+add_action( 'wp_ajax_the_ajax_hook', 'the_action_function' );
+add_action( 'wp_ajax_nopriv_the_ajax_hook', 'the_action_function' );
 /**
- * This function should be called by cron. It checks the spot api for new waypoints. if there are new points they are stored to the db.
- * NOTE: The spot api shouldn't be called more often than 150sec otherwise the servers ip will be blocked.
+ * The function gets called when a client wants to view the spotmap
+ */
+function the_action_function(){
+	/* this area is very simple but being serverside it affords the possibility of retreiving data from the server and passing it back to the javascript function */
+	wp_send_json(spotmap_get_data());
+}
+
+
+add_action( 'spotmap_cron_hook', 'spotmap_cron_exec',10,0 );
+/**
+ * This function gets called by cron. It checks the SPOT API for new data. If so the GeoJSON gets updated.
+ * NOTE: The SPOT API shouldn't be called more often than 150sec otherwise the servers ip will be blocked.
  */
 function spotmap_cron_exec(){
     //get feed
+    //TODO: should be editable in backend
     $spotmap_feed = '0XgPnzRoTYnfT09sX5LGl2vXsyfF3nsm6';
     $feedurl = 'https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/'.$spotmap_feed.'/message.json';
     $jsonraw = file_get_contents($feedurl);
 	$json = json_decode($jsonraw,true)['response']['feedMessageResponse'];
 	$messages_count = $json['count'];
 	$messages = $json['messages']['message'];
+	#loop through the data, if a msg is in the db all the others are there as well
+	$update = false;
 	global $wpdb;
-	foreach($messages as $msg){
+	foreach((array)$messages as $msg){
 		if(is_point_in_db($msg['id'])){
 		  break;
 		}
@@ -100,76 +122,65 @@ function spotmap_cron_exec(){
 			)
 		);
 	}
-	spotmap_update_kml();
 }
 
-add_action( 'spotmap_cron_hook', 'spotmap_cron_exec',10,0 );
-
-function spotmap_update_kml(){
-
-    $kml ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<kml xmlns=\"http://earth.google.com/kml/2.0\">
-<Document>
-      <name>Spot Track</name>
-      <Style id=\"Track\">
-         <LineStyle>
-            <color>ff0000f9</color>
-            <width>2</width>
-         </LineStyle>
-         <PolyStyle>
-            <color>ff0000f9</color>
-         </PolyStyle>
-      </Style>";
-
+function spotmap_get_data(){
 	global $wpdb;
 	$points = $wpdb->get_results("SELECT id, message_type, time, longitude, latitude, altitude FROM " . $wpdb->prefix . "spotmap_points;");
 
-	$coordinates='';
-	foreach ($points as $point){
-		// Creates a Placemark and append it to the Document.
-        $kml.='<Placemark id="'.$point->id.'">
-        <name>'.$point->message_type.'</name>
-        <description>Time: ' . gmdate('M d Y H:i:s',$point->time) . '</description>
-        <Point>
-          <coordinates>'.$point->longitude.','.$point->latitude.',0</coordinates>
-        </Point>
-      </Placemark>';
-        $coordinates.=$point->longitude.','.$point->latitude.',0
-        ';
-    }
+	$data = array();
+	$daycoordinates = array();
+	$lasttime = null;
+	foreach ($points as $key => $point){
+		$newpoint = new stdClass();
+		$newpoint->type = 'Feature';
 
-    $kml.='<Placemark>
-         <name>Spot Track</name>
-         <styleUrl>#Track</styleUrl>
-         <LineString>
-            <tessellate>1</tessellate>
-            <altitudeMode>clampToGround</altitudeMode>
-			   <coordinates>'.$coordinates.'</coordinates>
-   		</LineString>
-   	</Placemark>';
-    $kml.='</Document></kml>';
+		$geometry = new stdClass();
+		$geometry->type = 'Point';
+		$geometry->coordinates = array($point->longitude,$point->latitude);
+		$newpoint->geometry=$geometry;
 
-	$file = fopen('spotmap.kml','w');
-	fwrite($file,$kml);
-	fclose($file);
+		$properties = new stdClass();
+		$properties->id = $point->id;
+		$properties->type = $point->message_type;
+		$properties->time = $friendly_date = date_i18n( get_option('date_format'), $point->time );;
+		$newpoint->properties = $properties;
+
+
+		//TODO create a line between the points in chronological order
+		/*if (($point->time - $lasttime) <= 43200){
+			$daycoordinates[] = $geometry->coordinates;
+		} else if (count($daycoordinates) > 1){
+			$dayline = new stdClass();
+			$dayline->type = "LineString";
+			$dayline->coordinates = $daycoordinates;
+			$data[] = $dayline;
+			$daycoordinates = array();
+		}
+		$lasttime = $point->time;*/
+		$data[] = $newpoint;
+	}
+
+	return $data;
 }
 
-
+add_shortcode( 'spotmap', 'spotmap_show' );
+/**
+ * @param $atts  height and weight of the map
+ * @return string
+ */
 function spotmap_show( $atts ) {
-	$a = shortcode_atts( array(
-		'width' => '400',
-		'height' => '400',
+	wp_enqueue_style('leafletcss');
+	#if no attributes are provided use the default:
+    $a = shortcode_atts( array(
+		'height' => '400'
 	), $atts );
 
-	$spotmap_kml_url = "http://2-camp.com/spotmap.kml";//urlencode(site_url()."/spotmap.kml");
-	$spotmap_iframe = "<iframe width=\"".$a['width']."\" height=\"".$a['height']."]\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"http://www.topomap.co.nz/NZTopoMapEmbedded?v=2&z=12&kml=$spotmap_kml_url\"></iframe>";
-	return $spotmap_iframe;
+	return '<div id="spotmap" style="height: '.$a['height'].'px;"></div>';
 }
-add_shortcode( 'spotmap', 'spotmap_show' );
-
 
 /**
- * This function checks wether a point stored in the db or not
+ * This function checks whether a point stored in the db or not
  * @param $id The id of the point to check
  *
  * @return bool true if point with same id is in db else false
