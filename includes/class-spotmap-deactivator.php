c01df2953efe5badeb4da4e5640f282806f595ff
Timo Giese <giese.timo@gmail.com>
2020-06-04 19:22:02 +0200

    move actions from deactivate to deinstall

--- includes/class-spotmap-deactivator.php
+++ includes/class-spotmap-deactivator.php
@@ -7,19 +7,5 @@ class Spotmap_Deactivator {
 	public static function deactivate() {
 		//stop checking for new data from the feed
 		wp_unschedule_event( time(), 'spotmap_cron_hook' );
-
-		foreach (get_option("spotmap_options") as $key => $count) {
-			if($count < 1)
-				continue;
-			
-			for ($i=0; $i < $count; $i++) {
-				delete_option('spotmap_'.$key.'_name'.$i);
-				delete_option('spotmap_'.$key.'_id'.$i);
-				delete_option('spotmap_'.$key.'_password'.$i);
-			}
-		}
-		delete_option("spotmap_options");
-		
-		// tbd: delete db as well?
 	}
 }
